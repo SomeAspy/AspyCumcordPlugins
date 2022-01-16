102282a3b5f60da763ba22813a2e8057f1628983
Aiden <amb@aspy.dev>
2022-01-16 11:56:48 -0500

    small changes

--- NekosDotLife/api.js
+++ NekosDotLife/api.js
@@ -4,6 +4,9 @@
 // https://opensource.org/licenses/MIT
 import endpoints from './endpoints.json';
 export async function getNeko(name){
+    if(!endpoints[name]){
+        throw new Error(`Invalid endpoint: ${name}`);
+    };
     const resp=await fetch('https://nekos.life/api/v2'+endpoints[name]);
     return(await resp.json()).url;
 };
